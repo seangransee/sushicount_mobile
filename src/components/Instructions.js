7396ff674bc3f4218868568a979d9e059f75cd90
Sean Gransee <sean.gransee@gmail.com>
2017-12-21 19:23:44 -0600

    cleanup and vscode integration

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -3,9 +3,6 @@ import { Image } from "react-native"
 
 class Instructions extends React.Component {
   render() {
-    const INSTRUCTIONS_WIDTH = 601
-    const INSTRUCTIONS_HEIGHT = 203
-    const scale = 0.58
     return (
       <Image
         source={require("../img/instructions.png")}
